d4ed04f9e4312ea06628818650a9d5db9c5e8694
mozzi-00 <sunho0842@gmail.com>
2025-06-25 16:43:25 +0900

    Add files via upload
    최근에 올린 두 파일만 봐주세여

---
+++ Public/MyActor.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class HW05_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	FVector2D start = FVector2D(0, 0);
+
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+	
+	void move();
+	
+
+	int32 step();
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
